78623d9779aad0f61681a16f74460d98f59cfa42
Ricardo Casares <ricardocasares@gmail.com>
2018-10-19 01:22:06 +0200

    Fix trailing comma

--- components/Logic/Deduction/Proposition.js
+++ components/Logic/Deduction/Proposition.js
@@ -1,28 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Syllogism from '../Syllogism';
-import Analysis from './Analysis';
-import styles from './styles.css';
+import Syllogism from "../Syllogism";
+import Analysis from "./Analysis";
+import styles from "./styles.css";
 
-export default function Proposition({
-  id,
-  interfaceLanguage,
-  ...syllogism,
-}) {
+export default function Proposition({ id, interfaceLanguage, ...syllogism }) {
   return (
-    <div
-      key={ id }
-      className={ styles.Proposition }
-    >
-      <Syllogism
-        id={ id }
-        interfaceLanguage={ interfaceLanguage }
-        { ...syllogism }
-      />
-      <Analysis
-        interfaceLanguage={ interfaceLanguage }
-        syllogism={ syllogism }
-      />
+    <div key={id} className={styles.Proposition}>
+      <Syllogism id={id} interfaceLanguage={interfaceLanguage} {...syllogism} />
+      <Analysis interfaceLanguage={interfaceLanguage} syllogism={syllogism} />
     </div>
   );
 }
