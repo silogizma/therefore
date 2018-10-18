7729df2b3c4f96a0cb2fefcce1bba441a36f7bd1
Ricardo Casares <ricardocasares@gmail.com>
2018-10-19 01:23:39 +0200

    Format

--- components/UI/Layout/index.js
+++ components/UI/Layout/index.js
@@ -1,86 +1,61 @@
-import React, { Component } from 'react';
-import Link from 'next/link'
+import React, { Component } from "react";
+import Link from "next/link";
 
-import translate from '../../../i18n/translate';
-import styles from './styles.css';
+import translate from "../../../i18n/translate";
+import styles from "./styles.css";
 
 export default class Layout extends Component {
-  componentWillMount() {
-    if (typeof window === 'object') {
-      console.log(window.location.href)
-    }
-  }
-
   render() {
     const { children, page } = this.props;
-    const lang='tr'
+    const lang = "tr";
     return (
-      <div className={ styles.grid }>
-        <h1 className={ styles.logo }>
-          <Link
-            href={'/'}
-          >
+      <div className={styles.grid}>
+        <h1 className={styles.logo}>
+          <Link href={"/"}>
             <a>
               <img
-                height={ 60 }
-                width={ 60 }
-                alt={ 'silogizma' }
+                height={60}
+                width={60}
+                alt={"silogizma"}
                 src="/static/logo.png"
               />
             </a>
           </Link>
         </h1>
-        <nav className={ styles.preferences }>
+        <nav className={styles.preferences}>
           <span>interface languages: tr, en</span>
           <span>i am a color blind</span>
         </nav>
-        <ul className={ styles.nav }>
+        <ul className={styles.nav}>
           <li>
-            <Link
-              href="/"
-            >
-              <a
-                className={ page === 'home' && styles.currentLink }
-              >
-                { translate(lang, 'home') }
+            <Link href="/">
+              <a className={page === "home" && styles.currentLink}>
+                {translate(lang, "home")}
               </a>
             </Link>
           </li>
           <li>
-            <Link
-              prefetch
-              href='/definitions'
-            >
-              <a
-                className={ page === 'definitions' && styles.currentLink }
-              >
-                { translate(lang, 'definitions') }
+            <Link prefetch href="/definitions">
+              <a className={page === "definitions" && styles.currentLink}>
+                {translate(lang, "definitions")}
               </a>
             </Link>
           </li>
           <li>
-            <Link
-              prefetch href="/submit"
-            >
-              <a
-                className={ page === 'submit' && styles.currentLink }
-              >
-                { translate(lang, 'submit') }
+            <Link prefetch href="/submit">
+              <a className={page === "submit" && styles.currentLink}>
+                {translate(lang, "submit")}
               </a>
             </Link>
           </li>
         </ul>
-        <section className={ styles.content }>
-          { children }
-        </section>
+        <section className={styles.content}>{children}</section>
         <footer>
           <p>
             ...
             <br />
-            <a
-              href="//github.com/silogizma/therefore"
-            >
-              { translate(lang, 'read readme.md on github') }
+            <a href="//github.com/silogizma/therefore">
+              {translate(lang, "read readme.md on github")}
             </a>
           </p>
         </footer>
